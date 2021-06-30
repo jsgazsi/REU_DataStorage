c5647fbd60866c47de931ba6c16340b69ae281e1
jsgazsi <jsgazsi@gmail.com>
2021-06-30 16:55:13 -0400

    added nbproject to .gitignore

---
+++ src/datastorage/DataStorage.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datastorage;
+
+/**
+ *
+ * @author PID# 6224488
+ */
+public class DataStorage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+    
+}
