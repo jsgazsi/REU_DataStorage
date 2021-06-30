9e4ff0ffebe3870c0786d33a495f9e2b13d0532b
jsgazsi <jsgazsi@gmail.com>
2021-06-30 17:07:22 -0400

    Test update to Justin Branch

--- src/datastorage/DataStorage.java
+++ src/datastorage/DataStorage.java
@@ -15,7 +15,7 @@ public class DataStorage {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello World 2");
     }
     
 }
