6a145e3183c41e1d612d94dd86649f8bfce358b6
jsgazsi <jsgazsi@gmail.com>
2021-07-06 18:46:28 -0400

    Nodes have own distinct copies of blockchain

--- src/DataStorage.java
+++ src/DataStorage.java
@@ -6,100 +6,100 @@ import java.util.LinkedList;
  *
  * @author Justin Gazsi
  */
-
 public class DataStorage {
 
-    //Global ArrayList (Blockchain)
-    //public static ArrayList<Block> blockchain = new ArrayList<Block>();
-    public static LinkedList<Block> blockchain = new LinkedList<Block>();
     static int NUM_BLOCKS = 10;
+    static int NODE_CNT = 0;
+
+    //Network list of attached nodes
+    public static ArrayList<Node> Nodes = new ArrayList<Node>();
+    public static ArrayList<Block> publicBlockchain = new ArrayList<Block>();
 
     //MAIN DRIVER
-    public static void main(String[] args) {
-        //Create Genesis Block
-        //blockchain.add(new Block("Genesis Block", "0"));
-        blockchain.add(new Block("Genesis Block"));
+    public static void main(String[] args) throws InterruptedException {
 
-        //add some more blocks
-        for (int i = 2; i <= NUM_BLOCKS; i++) {
-            String data = "Block# " + String.valueOf(i) + " - Tx's data stub";
-            //Uncomment to tamper with blockchain, insert invalid block
+        //Create Genesis Block
+        publicBlockchain.add(new Block("Genesis Block", "0"));
+        //publicBlockchain.add(new Block("Block 2", publicBlockchain.get(publicBlockchain.size() - 1).hash));
+
+//        //add some more blocks
+//        for (int i = 2; i <= NUM_BLOCKS; i++) {
+//            String data = "Block# " + String.valueOf(i) + " - Tx's data stub";
+        //Uncomment to tamper with blockchain, insert invalid block
 //            if (i == 5){
 //                 blockchain.add(new Block(data, "Tampered here"));
 //            }
-           
-            //blockchain.add(new Block(data, blockchain.get(blockchain.size() - 1).hash));
-            blockchain.add(new Block(data));
+//           
+//            //blockchain.add(new Block(data, blockchain.get(blockchain.size() - 1).hash));
+//            blockchain.add(new Block(data));
+//        }
+        Nodes.add(new Node());
+        Nodes.get(0).generateBlock();
+        Nodes.get(0).generateBlock();
+        Nodes.get(0).generateBlock();
+
+        Nodes.add(new Node());
+        Thread.sleep(1000);
+        Nodes.get(1).generateBlock();
+        //Nodes.get(1).generateBlock();
+
+        System.out.println(Nodes.get(0).getNodeID());
+        System.out.println(Nodes.get(1).getNodeID());
+        System.out.println("");
+        //System.out.println(Nodes.size());
+        //Nodes.get(0).generateBlock();
+
+//        Node node1 = new Node();
+//        node1.generateBlock();
+//        node1.generateBlock();   
+//        
+//        Node node2 = new Node();
+//        node2.generateBlock();
+//        Node node3 = new Node();
+//        System.out.println("NodeID: " + node1.nodeID + " Quorum Group: " + node1.quorumID);
+//        System.out.println("NodeID: " + node2.nodeID + " Quorum Group: " + node2.quorumID);
+//        System.out.println("NodeID: " + node3.nodeID + " Quorum Group: " + node3.quorumID);
+//        System.out.println("");
+//        
+        //Verify blockchain
+        //System.out.println("Is blockchain valid: " + node1.isChainValid() + "\n");
+        //mint new block to node1 blockchain and print
+//      node1.blockchain.add(new Block("Test ADD TX"));
+
+        System.out.println("NodeID: " + Nodes.get(0).getNodeID());
+        for (Block block : Nodes.get(0).getBlockchain()) {
+            System.out.println("TimeStamp: " + block.timeStamp);
+            System.out.println("Data: " + block.data);
+            System.out.println("Prev Hash: " + block.previousHash);
+            System.out.println("Hash: " + block.hash);
+            System.out.println("");
+            //System.out.println(node1.blockchain.size());
+        }
+//        
+        System.out.println("NodeID: " + Nodes.get(1).getNodeID());
+        for (Block block : Nodes.get(1).getBlockchain()) {
+            System.out.println("TimeStamp: " + block.timeStamp);
+            System.out.println("Data: " + block.data);
+            System.out.println("Prev Hash: " + block.previousHash);
+            System.out.println("Hash: " + block.hash);
+            System.out.println("");
         }
 
         //Print Blockchain info
-        for (Block block : blockchain) {
-            System.out.println("TimeStamp: " + block.timeStamp);
-            System.out.println(block.data);
-            System.out.println("Prev Hash: " + block.previousHash);
-            System.out.println("Hash: " + block.hash);
-            System.out.println("");
-        }
-        
-        System.out.println("Is blockchain valid: " + isChainValid() + "\n");
-        
-        Node node1 = new Node();
-        Node node2 = new Node();
-        System.out.println("NodeID: " + node1.nodeID + " Quorum Group: " + node1.quorumID);
-        System.out.println("NodeID: " + node2.nodeID + " Quorum Group: " + node2.quorumID);
-        System.out.println("");
-        
-        //mint new block to node1 blockchain and print
-        node1.blockchain.add(new Block("Test TX"));
-        for (Block block : node1.blockchain) {
-            System.out.println("TimeStamp: " + block.timeStamp);
-            System.out.println(block.data);
-            System.out.println("Prev Hash: " + block.previousHash);
-            System.out.println("Hash: " + block.hash);
-            System.out.println("");
-        }
-        
+        System.out.println("--------------------------------");
+//        for (Block block : DataStorage.blockchain) {
+//            System.out.println("TimeStamp: " + block.timeStamp);
+//            System.out.println(block.data);
+//            System.out.println("Prev Hash: " + block.previousHash);
+//            System.out.println("Hash: " + block.hash);
+//            System.out.println("");
+//        }
+
         //Testing some output
-        Transaction tx = node1.createTransaction();
+        Transaction tx = Nodes.get(0).createTransaction();
         System.out.println("Node ID: " + tx.nodeID + "\n" + 
                 "TimeStamp: " + tx.timeStamp + "\n" + 
                 "TX ID: " + tx.TxID);
-
-
-    }
-
-    
-    
-    
-    
-    // Function to check validity of the blockchain
-    public static Boolean isChainValid() {
-        
-        Block currentBlock;
-        Block previousBlock;
-
-        // Iterating through all the blocks
-        for (int i = 1; i < blockchain.size(); i++) {
-
-            // Storing the current block and the previous block
-            currentBlock = blockchain.get(i);
-            previousBlock = blockchain.get(i - 1);
-
-            // Checking if the current hash is equal to the calculated hash or not
-            if (!currentBlock.hash.equals(currentBlock.calculateHash())) {
-                System.out.println("Hashes are not equal");
-                return false;
-            }
-
-            // Checking of the previous hash is equal to the calculated previous hash or not
-            if (!previousBlock.hash.equals(currentBlock.previousHash)) {
-                System.out.println("Previous Hashes are not equal");
-                return false;
-            }
-        }
-
-        // If all the hashes are equal to the calculated hashes, then the blockchain is valid
-        return true;
     }
 
 }
